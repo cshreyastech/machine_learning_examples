89aa2778f59903328dce40acb7a5c092408f38c5
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2023-11-27 17:31:36 -0800

    02_10_epsilon_greedy_starer.py before fixing

---
+++ r1/02_10_epsilon_greedy_starer.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Bandit:
+  def __init__(self, p):
+    self.p = p
+    self.p_estimate = #TODO
+    self.N = #TODO
+
+  def pull(self):
+    # draw a 1 with probability p
+    return np.random.randn() < self.p
+
+  def update(self, x):
+    self.N = # TODO 
+    self.p_estimate = # TODO
+
+
+def experiment():
+  bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+
+  rewards = np.zeros(NUM_TRIALS)
+
+  num_times_explored = 0
+  num_times_exploited = 0
+  num_optimal = 0
+  optimal_j = np.argmax([b.p for b in bandits])
+  print("optimal j:", optimal_j)
+
+  for i in range(NUM_TRIALS):
+    # epsilon greedy to select the next bandit
+    if np.random.random() < EPS:
+      num_times_explored += 1
+      j = # TODO
+    else:
+      num_times_exploited += 1
+      j = # TODO
+
+    if j == optimal_j:
+      num_optimal += 1
+
+    # pull the arm for the bandit with the largest sample
+    x = bandits[j].pull()
+
+    # update rewards logs
+    rewards[i] = x
+
+    # update the distribution for the bandit whose arm we just pulled
+    bandits[j].update(x)
+
+
+  # print mean estimates for each bandit
+  for b in bandits:
+    print("mean estimate:", b.p_estimate)
+
+  # print total reward
+  print("total reward earned               :", reward.sum())
+  print("overall win rate                  :", reward.sum() / NUM_TRIALS)
+  print("num_times_explored                :", num_times_explored)
+  print("num_times_exploited               :", num_times_exploited)
+  print("num times selected optimal bandits:", num_optimal)
+
+  # plot the results
+  cumulative_rewards = np.cumsum(rewards)
+  win_rates = cumulative_rewards / (np.arange(NUM_TRIALS) + 1)
+  plt.plot(win_rates)
+  plt.plot(np.ones(NUM_TRIALS) * np.max(BANDIT_PROBABILITIES))
+  plt.show()
+
+if __name__ == "__main__":
+  experiment()
